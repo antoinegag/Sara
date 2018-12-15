e78e58a4e10f7af0a19c1c716d78b6b2d7cb63dc
Antoine Gagnon <antoine.gagnon760@hotmail.com>
2018-12-15 17:04:29 -0500

    add boilerplate

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+router.get("/", (req, res) => {
+    return res.json({ success: true});
+});
+
+// append /api for our http requests
+app.use("/api", router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  // initialize our state 
+  state = {
+    result: null
+  };
+
+  componentDidMount() {
+    fetch('/api/').then(data => data.json()).then(res => this.setState({result: res}));
+  }
+
+  render() {
+    const { result } = this.state;
+    if(!result) {
+      return (<div>Loading...</div>);
+    } else {
+      return (
+        <div>
+          Success: {JSON.stringify(result)}
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
