6da27360724057b7ac3f182b2930c862030166cf
Antoine Gagnon <antoine.gagnon760@hotmail.com>
2018-12-15 19:41:24 -0500

    Remove old setup

--- setup.sh
+++
@@ -1,8 +0,0 @@
-git pull
-echo Installing server packages
-cd backend && npm install
-echo Installing client packages
-cd ../client && npm install
-echo Building app
-npm run build
-echo Run NODE_ENV=production if you are in production
