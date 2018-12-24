6ca6eb63dfd10f67a0b68b4093de6f8a0276765f
Antoine Gagnon <antoine.gagnon760@hotmail.com>
2018-12-24 15:00:28 -0500

    Allow for remote arduino connect/disconnect/reconnect

--- backend/api/system/arduino.js
+++ backend/api/system/arduino.js
@@ -6,4 +6,19 @@ router.get('/', (req, res) => {
   return res.json({online: arduino.isReady()});
 });
 
+router.post('/reconnect', (req, res) => {
+  arduino.reset();
+  return res.json({success: true});
+});
+
+router.post('/connect', (req, res) => {
+  arduino.connect();
+  return res.json({success: true});
+});
+
+router.post('/disconnect', (req, res) => {
+  arduino.disconnect();
+  return res.json({success: true});
+});
+
 module.exports = router;
--- backend/arduino/serial.js
+++ backend/arduino/serial.js
@@ -2,11 +2,11 @@ const SerialPort = require('serialport');
 const Delimiter = require('@serialport/parser-delimiter');
 
 const address = process.env.ARDUINO_PORT || '/dev/ttyUSB0';
-const port = new SerialPort(address);
-console.info(`Arduino port set to ${address}`);
 
-const parser = port.pipe(new Delimiter({delimiter: '\r\n'}));
+let port = new SerialPort(address);
+console.info(`Connecting to arduino @ ${address}`);
 
+let parser = port.pipe(new Delimiter({delimiter: '\r\n'}));
 let ready = false;
 let waitingForData = false;
 
@@ -23,31 +23,36 @@ let waitingForData = false;
  */
 const queue = [];
 
+parserHandler();
+
 /**
  * Handle data input from Arduino
  */
-parser.on('data', (data) => {
-  const string = data.toString();
-  if (ready) {
-    if (waitingForData) {
-      const entry = queue[0];
-      entry.resolve(string);
-      waitingForData = false;
-      queue.shift();
-      processNext();
+function parserHandler() {
+  parser.on('data', (data) => {
+    const string = data.toString();
+    if (ready) {
+      if (waitingForData) {
+        const entry = queue[0];
+        entry.resolve(string);
+        waitingForData = false;
+        queue.shift();
+        processNext();
+      } else {
+        console.warn(`${new Date()} - Got unexpected data: ${string} (ignored)`);
+      }
     } else {
-      console.warn(`${new Date()} - Got unexpected data: ${string} (ignored)`);
+      if (string == 'READY') {
+        ready = true;
+        console.info(`Arduino @ ${address} ready`);
+        processNext();
+      } else {
+        console.warn(`Received data "${string}" but Arduino is not ready`);
+      }
     }
-  } else {
-    if (string == 'READY') {
-      ready = true;
-      console.info(`Arduino @ ${address} ready`);
-      processNext();
-    } else {
-      console.warn(`Received data "${string}" but Arduino is not ready`);
-    }
-  }
-});
+  });
+};
+
 
 /**
  * Process the next command in queue
@@ -66,6 +71,40 @@ function processNext() {
     entry.resolve();
     processNext();
   }
+}
+
+/**
+ * Connect to the arduino
+ */
+function connect() {
+  if (ready) {
+    console.error(`Attempt to connect to arduino but already connected, call reconnect to reset the connection`);
+    return;
+  }
+  ready = false;
+  waitingForData = false;
+  port = new SerialPort(address);
+  console.info(`Connecting to arduino @ ${address}`);
+  parser.removeAllListeners();
+  parser = port.pipe(new Delimiter({delimiter: '\r\n'}));
+  parserHandler();
+}
+
+/**
+ * Close the serial port
+ */
+function disconnect() {
+  port.close();
+  ready = false;
+  console.info(`Disconnected from ${address}`);
+}
+
+/**
+ * Disconnect and connect to the port
+ */
+function reset() {
+  disconnect();
+  connect();
 }
 
 /**
@@ -107,4 +146,7 @@ module.exports = {
     return ready;
   },
   port: port,
+  reset: reset,
+  disconnect: disconnect,
+  connect: connect,
 };
