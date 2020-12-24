86033f0255db3f0ccfae5395127b4723dcc65310
Antoine <antoine.gagnon760@gmail.com>
2020-12-23 20:07:26 -0500

    Allow cors

--- hardware-service/src/app.ts
+++ hardware-service/src/app.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { config } from "dotenv";
 import { json } from "body-parser";
 import APIRouter from "./api";
+import cors from "cors";
+
 config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(json());
+app.use(cors());
 
 app.use(APIRouter);
 
